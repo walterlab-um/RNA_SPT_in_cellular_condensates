a61ccd33e6ff53ed91ec1bce9b4be13413353983
Guoming <51248407+Guoming-Gao@users.noreply.github.com>
2023-06-22 11:03:32 -0400

    For cell body and HOPS condensate recognition
    average projection for cell body recognition; first frame for condensate recognition

--- ilastik_random_pick_livecell.py
+++ ilastik_random_pick_livecell.py
@@ -1,18 +1,29 @@
 import os
-from os.path import join, isfile
-import shutil
+from os.path import join
+import numpy as np
+from tifffile import imread, imwrite
 from random import choices
+from rich.progress import track
 
-# This script randomly picks 5 FOV from each condition to pool a ilastik traking dataset, to tranin a machine learning model for condensate boundary detection.
+# This script randomly picks 7 FOV from each condition to pool a ilastik traking dataset, to tranin a machine learning model for condensate boundary detection.
 
-folder_from = "/Volumes/AnalysisGG/PROCESSED_DATA/RNA_SPT_in_FUS-May2023_wrapup/Condensates_AveProj"
-folder_to = "/Volumes/AnalysisGG/PROCESSED_DATA/Training-ilastik/FUS_invitro_20ms"
+dir_from = "/Volumes/AnalysisGG/PROCESSED_DATA/RNA_in_HOPS-Jun2023_wrapup/LiveCell-different-RNAs-in-HOPS"
+dir_HOPS_condensate = (
+    "/Volumes/AnalysisGG/PROCESSED_DATA/Training-ilastik/livecell_100ms-Dcp1a_HOPS"
+)
+dir_cell_body = "/Volumes/AnalysisGG/PROCESSED_DATA/Training-ilastik/livecell_100ms-cell_body_byDcp1a"
 
-lst_subfolders = [f for f in os.listdir(folder_from) if not isfile(f)]
+lst_subfolders = [f for f in os.listdir(dir_from) if f.endswith("100ms")]
 
-os.chdir(folder_from)
-for subfolder in lst_subfolders:
-    all_files_in_subfolder = [f for f in os.listdir(subfolder) if f.endswith(".tif")]
-    chosen_ones = choices(all_files_in_subfolder, k=5)
+os.chdir(dir_from)
+for subfolder in track(lst_subfolders):
+    all_files_in_subfolder = [
+        f for f in os.listdir(subfolder) if f.endswith("left.tif")
+    ]
+    chosen_ones = choices(all_files_in_subfolder, k=7)
     for fname in chosen_ones:
-        shutil.copy(join(folder_from, subfolder, fname), join(folder_to, fname))
+        video = imread(join(dir_from, subfolder, fname))
+        first_frame = video[0, :, :].astype("uint16")
+        average_projection = np.mean(video, axis=0).astype("uint16")
+        imwrite(join(dir_HOPS_condensate, fname), first_frame, imagej=True)
+        imwrite(join(dir_cell_body, fname), average_projection, imagej=True)
