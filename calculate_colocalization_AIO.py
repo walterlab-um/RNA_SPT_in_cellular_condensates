a46644e28f42b59ea13fe39ef7902a2e977b84fc
Guoming <51248407+Guoming-Gao@users.noreply.github.com>
2023-07-02 00:36:30 -0400

    Update calculate_colocalization_AIO.py

--- calculate_colocalization_AIO.py
+++ calculate_colocalization_AIO.py
@@ -1,9 +1,8 @@
 import os
-from os.path import join, dirname, basename
-import scipy.stats as stats
+from os.path import join, dirname
 import numpy as np
 import pandas as pd
-from tkinter import filedialog as fd
+from shapely.geometry import Point, Polygon
 from rich.progress import track
 
 pd.options.mode.chained_assignment = None  # default='warn'
@@ -33,16 +32,11 @@ lst_subfolders = [f for f in os.listdir(dir_from) if f.endswith("100ms")]
 
 
 # Output file columns
-angle_bins = np.linspace(0, 180, 7).astype(int)  # #boundaries = #bins + 1
-lst_angle_fraction_titles = [
-    "(" + str(angle_bins[i]) + "," + str(angle_bins[i + 1]) + "]"
-    for i in range(len(angle_bins) - 1)
-]
 columns = [
-    "filename_RNA",
-    "filename_condensate",
-    "filename_cell",
-    "trackID",
+    "fname_RNA",
+    "fname_condensate",
+    "fname_cell",
+    "RNA_trackID",
     "t",
     "x",
     "y",
@@ -53,87 +47,98 @@ columns = [
 ]
 
 
+def list_like_string_to_polygon(list_like_string):
+    # example list_like_string structure of polygon coordinates: '[[196, 672], [196, 673], [197, 673], [198, 673], [199, 673], [199, 672], [198, 672], [197, 672]]'
+    list_of_xy_string = list_like_string[2:-2].split("], [")
+
+    coords_roi = []
+    for xy_string in list_of_xy_string:
+        x, y = xy_string.split(", ")
+        coords_roi.append((int(x), int(y)))
+
+    polygon_output = Polygon(coords_roi)
+
+    return polygon_output
+
+
+def list_like_string_to_xyt(list_like_string):
+    # example list_like_string structure of xyt: '[0, 1, 2, 3]'
+    list_of_xyt_string = list_like_string[1:-1].split(", ")
+    lst_xyt = []
+    for xyt_string in list_of_xyt_string:
+        lst_xyt.append(float(xyt_string))
+
+    return lst_xyt
+
+
 lst_rows_of_df = []
-print("Now Processing:", dirname(lst_fpath[0]))
-for fpath in track(lst_fpath):
-    df_current_file = pd.read_csv(fpath, dtype=float)
-    df_current_file = df_current_file.astype({"t": int})
-    fname = basename(fpath)
-    lst_trackID_in_file = df_current_file.trackID.unique().tolist()
+os.chdir(dir_from)
+## loop through each subfolder/experiment condition
+for subfolder in lst_subfolders:
+    all_fname_cell_body = [
+        f for f in os.listdir(join(subfolder, "cell_body")) if f.endswith(".txt")
+    ]
+    all_fname_RNA_AIO = [
+        f for f in os.listdir(join(subfolder, "RNA")) if f.startswith("SPT_results_AIO")
+    ]
+    all_fname_condensate_AIO = [
+        f
+        for f in os.listdir(join(subfolder, "condensate"))
+        if f.startswith("condensates_AIO")
+    ]
 
-    for trackID in lst_trackID_in_file:
-        df_current_track = df_current_file[df_current_file.trackID == trackID]
-        tracklength = df_current_track.shape[0]
-        if tracklength < tracklength_threshold:
-            continue
+    ## loop through each FOV
+    for fname_RNA_AIO in all_fname_RNA_AIO:
+        df_RNA = pd.read_csv(join(subfolder, "RNA", fname_RNA_AIO))
 
-        x = df_current_track["x"].to_numpy(dtype=float)
-        y = df_current_track["y"].to_numpy(dtype=float)
-        disp_um = np.sqrt((x[-1] - x[0]) ** 2 + (y[-1] - y[0]) ** 2) * um_per_pixel
+        # find the fnames for cells within the current FOV
+        fnames_cells_in_current_FOV = []
+        for f in all_fname_cell_body:
+            if fname_RNA_AIO[16:].split("-RNA")[0] in f:
+                fnames_cells_in_current_FOV.append(f)
+                break
+        if len(fnames_cells_in_current_FOV) == 0:
+            print("There's no cells in current FOV.")
+            break
 
-        lags = np.arange(1, tracklength - 2)
-        lags_phys = lags * s_per_frame
-        MSDs = calc_MSD_NonPhysUnit(df_current_track, lags)
-        if np.any(MSDs == 0):  # remove any track with zero MSD
-            continue
-        MSDs_phys = MSDs * (um_per_pixel**2)  # um^2
+        # load the condensates AIO file for the current FOV
+        for f in all_fname_condensate_AIO:
+            if fname_RNA_AIO[16:].split("-RNA")[0] in f:
+                fname_condensate_AIO = f
+                break
+        if "fname_condensate_AIO" not in globals():
+            print("There's no condensate AIO file for the current FOV.")
+            break
+        df_condensate = pd.read_csv(join(subfolder, "condensate", fname_condensate_AIO))
 
-        # D formula with errors (MSD: um^2, t: s, D: um^2/s, n: dimension, R: motion blur coefficient; doi:10.1103/PhysRevE.85.061916)
-        # diffusion dimension = 2. Note: This is the dimension of the measured data, not the actual movements! Although particles are doing 3D diffussion, the microscopy data is a projection on 2D plane and thus should be treated as 2D diffusion!
-        # MSD = 2 n D tau + 2 n sigma^2 - 4 n R D tau, n=2, R=1/6
-        # Therefore, slope = (2n-4nR)D = (8/3) D; intercept = 2 n sigma^2
-        slope_linear, intercept_linear, R_linear, P, std_err = stats.linregress(
-            lags_linear * s_per_frame, MSDs_phys[: len(lags_linear)]
-        )
-        if (slope_linear > 0) & (intercept_linear > 0):
-            D_phys_linear = slope_linear / (8 / 3)  # um^2/s
-            log10D_linear = np.log10(D_phys_linear)
-            sigma_phys = np.sqrt(intercept_linear / 4) * 1000  # nm
-        else:
-            D_phys_linear = np.NaN
-            log10D_linear = np.NaN
-            sigma_phys = np.NaN
+        # process RNA tracks one by one
+        for trackID in df_RNA["trackID"]:
+            current_track = df_RNA[df_RNA["trackID"] == trackID]
+            lst_x = list_like_string_to_xyt(current_track["list_of_x"].squeeze())
+            lst_y = list_like_string_to_xyt(current_track["list_of_y"].squeeze())
+            lst_t = list_like_string_to_xyt(current_track["list_of_t"].squeeze())
 
-        # MSD = 2 n D tau^alpha = 4 D tau^alpha
-        # log(MSD) = alpha * log(tau) + log(D) + log(4)
-        # Therefore, slope = alpha; intercept = log(D) + log(4)
-        slope_loglog, intercept_loglog, R_loglog, P, std_err = stats.linregress(
-            np.log10(lags_loglog * s_per_frame),
-            np.log10(MSDs_phys[: len(lags_loglog)]),
-        )
-        log10D_loglog = intercept_loglog - np.log10(4)
-        alpha = slope_loglog
+            # process each position in track one by one
+            for i in len(lst_t):
+                t = lst_t[i]
+                x = lst_x[i]
+                y = lst_y[i]
 
-        angles = calc_angle(x, y)
-        densities, _ = np.histogram(np.absolute(angles), angle_bins, density=True)
-        # fractions are summed to 1; fraction = density * bin width
-        fractions = densities * (angle_bins[1] - angle_bins[0])
-
-        # Save
-        new_row = [
-            fname,
-            trackID,
-            df_current_track["t"].to_list(),
-            df_current_track["x"].to_list(),
-            df_current_track["y"].to_list(),
-            tracklength,
-            disp_um,
-            x.mean(),
-            y.mean(),
-            df_current_track["meanIntensity"].max(),  # max of mean spot intensity
-            MSDs_phys.tolist(),
-            lags_phys.tolist(),
-            slope_linear,
-            R_linear**2,
-            sigma_phys,
-            D_phys_linear,
-            log10D_linear,
-            R_loglog**2,
-            log10D_loglog,
-            alpha,
-            angles.tolist(),
-        ] + fractions.tolist()
-        lst_rows_of_df.append(new_row)
+                # Save
+                new_row = [
+                    fname_RNA_AIO,
+                    fname_condensate_AIO,
+                    "fname_cell",
+                    trackID,
+                    t,
+                    x,
+                    y,
+                    "InCondensate",
+                    "condensateID",
+                    "distance_to_center_nm",
+                    "distance_to_edge_nm",
+                ]
+                lst_rows_of_df.append(new_row)
 
 df_save = pd.DataFrame.from_records(
     lst_rows_of_df,
