d8a16f38983d48efb69541ef163cfeda36cdde7b
Guoming <51248407+Guoming-Gao@users.noreply.github.com>
2024-04-25 12:24:33 -0400

    Update calculate_N_condensate_per_cell.py

--- calculate_N_condensate_per_cell.py
+++ calculate_N_condensate_per_cell.py
@@ -9,22 +9,6 @@ import tkinter as tk
 pd.options.mode.chained_assignment = None  # default='warn'
 
 
-def transform_string(input_string):
-    # Split the input string by '-' to get the parts
-    parts = input_string.split("-")
-
-    # Remove the last part (e.g., 'cell1.txt')
-    parts.pop()
-
-    # Join the remaining parts back together with '-'
-    output_string = "-".join(parts)
-
-    # Add the prefix and postfix to the output string
-    output_string = "condensates_AIO-" + output_string + ".csv"
-
-    return output_string
-
-
 def count_within_cell_polygon(df, cell_polygon):
     """
     Take a Polygon cell_polygon and a dataframe contain columns 'x' and 'y', and return numpy array of x and y within the cell_polygon.
@@ -36,12 +20,28 @@ def count_within_cell_polygon(df, cell_polygon):
     return count
 
 
-# Function to process a single file
-def process_file(
-    i,
-    cell_roi_files,
-    folder_path,
-):
+print("Choose the main folder contains subfolders: condensate, cell_body_mannual")
+root = tk.Tk()
+root.withdraw()  # Hide the main window
+
+folder_path = fd.askdirectory()
+fname_save = "N_condensate_per_cell.csv"
+
+cell_roi_files = [
+    file
+    for file in os.listdir(join(folder_path, "cell_body_mannual"))
+    if file.endswith(".txt")
+]
+condensate_files = [
+    f
+    for f in os.listdir(join(folder_path, "condensate"))
+    if f.startswith("condensates_AIO")
+]
+
+
+lst_cell_roi = []
+lst_N_condensate_per_cell = []
+for i in track(range(len(cell_roi_files))):
     # import cell boundary as a polygon
     cell_roi_file = cell_roi_files[i]
     cell_outline_coordinates = pd.read_csv(
@@ -51,54 +51,23 @@ def process_file(
     cell_polygon = Polygon(coords_roi)
 
     # import condensates
+    matching_condensate_file = [
+        s for s in condensate_files if s.startswith(cell_roi_file.split("-cell")[0])
+    ][0]
     try:
         df_condensate = pd.read_csv(
-            join(folder_path, "condensate", transform_string(cell_roi_file))
+            join(folder_path, "condensate", matching_condensate_file)
         )
         N_condensate_per_cell = count_within_cell_polygon(df_condensate, cell_polygon)
     except FileNotFoundError:
-        N_condensate_per_cell = 0
+        continue
 
-    return (
-        cell_roi_file,
-        N_condensate_per_cell,
-    )
+    lst_cell_roi.append(cell_roi_file)
+    lst_N_condensate_per_cell.append(N_condensate_per_cell)
 
-
-def main():
-    print("Choose the main folder contains subfolders: condensate, cell_body_mannual")
-    root = tk.Tk()
-    root.withdraw()  # Hide the main window
-
-    folder_path = fd.askdirectory()
-    fname_save = "N_condensate_per_cell.csv"
-
-    cell_roi_files = [
-        file
-        for file in os.listdir(join(folder_path, "cell_body_mannual"))
-        if file.endswith(".txt")
-    ]
-
-    lst_cell_roi = []
-    lst_N_condensate_per_cell = []
-    for i in track(range(len(cell_roi_files))):
-        cell_roi_file, N_condensate_per_cell = process_file(
-            i,
-            cell_roi_files,
-            folder_path,
-        )
-        if N_condensate_per_cell == 0:
-            continue
-        lst_cell_roi.append(cell_roi_file)
-        lst_N_condensate_per_cell.append(N_condensate_per_cell)
-
-    df_save = pd.DataFrame(
-        {"cell_rois": lst_cell_roi, "N_condensate_per_cell": lst_N_condensate_per_cell}
-    )
-    df_save.to_csv(join(folder_path, fname_save), index=False)
-    print("Saved successfully at the following path:")
-    print(join(folder_path, fname_save))
-
-
-if __name__ == "__main__":
-    main()
+df_save = pd.DataFrame(
+    {"cell_rois": lst_cell_roi, "N_condensate_per_cell": lst_N_condensate_per_cell}
+)
+df_save.to_csv(join(folder_path, fname_save), index=False)
+print("Saved successfully at the following path:")
+print(join(folder_path, fname_save))
